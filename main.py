4d4e9260f6981cb4ad3931e1f0e92882f5264a3d
Vano56 <ivankandin56@gmail.com>
2022-01-21 10:31:53 +0500

    New new main

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ e = 'zzz'
 
 g = 'ddd'
 
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+
+
 print(a + b)
